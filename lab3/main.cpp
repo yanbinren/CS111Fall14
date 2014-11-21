c5dd69d8667decd8cd31b206f53814092e53f37d
Yanbin Ren <ybren@YanbinsMacBookPro.local>
2014-11-20 16:43:53 -0800

    lab3

---
+++ lab3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  lab3
+//
+//  Created by Yanbin Ren on 11/15/14.
+//  Copyright (c) 2014 Yanbin Ren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
